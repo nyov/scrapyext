40878ada7e4792397010c5764a22a358b8f241dc
nyov <nyov@nexnode.net>
2012-11-29 22:49:05 +0000

    remove duplicate

--- mongo.py
+++ mongo.py
@@ -2,7 +2,7 @@
 MongoDBStorage
 
 ITEM_PIPELINES = [
-    'project.pipelines.MongoDBStorage',
+	'project.pipelines.MongoDBStorage',
 ]
 
 MONGODB_SERVER = "localhost"
@@ -19,23 +19,23 @@ from scrapy.exceptions import DropItem
 
 
 class MongoDBStorage(object):
-    def __init__(self):
-        self.server = settings['MONGODB_SERVER']
-        self.port = settings['MONGODB_PORT']
-        self.db = settings['MONGODB_DB']
-        self.col = settings['MONGODB_COLLECTION']
-        connection = pymongo.Connection(self.server, self.port)
-        db = connection[self.db]
-        self.collection = db[self.col]
+	def __init__(self):
+		self.server = settings['MONGODB_SERVER']
+		self.port = settings['MONGODB_PORT']
+		self.db = settings['MONGODB_DB']
+		self.col = settings['MONGODB_COLLECTION']
+		connection = pymongo.Connection(self.server, self.port)
+		db = connection[self.db]
+		self.collection = db[self.col]
 
-    def process_item(self, item, spider):
-        err_msg = ''
-        for field, data in item.items():
-            if not data:
-                err_msg += 'Missing %s of item from %s\n' % (field, item['url'])
-        if err_msg:
-            raise DropItem(err_msg)
-        self.collection.insert(dict(item))
-        log.msg('Item written to MongoDB database %s/%s' % (self.db, self.col),
-                level=log.DEBUG, spider=spider)
-        return item
+	def process_item(self, item, spider):
+		err_msg = ''
+		for field, data in item.items():
+			if not data:
+				err_msg += 'Missing %s of item from %s\n' % (field, item['url'])
+		if err_msg:
+			raise DropItem(err_msg)
+		self.collection.insert(dict(item))
+		log.msg('Item written to MongoDB database %s/%s' % (self.db, self.col),
+			level=log.DEBUG, spider=spider)
+		return item
