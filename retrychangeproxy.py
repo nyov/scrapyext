51ebe5cdedf48624b264776fe2803128600d0299
nyov <nyov@nexnode.net>
2013-02-03 23:11:39 +0000

    update middleware

--- retrychangeproxy.py
+++ retrychangeproxy.py
@@ -1,20 +1,30 @@
 """
+RetryChangeProxyMiddleware
+
+DownloaderMiddleware that changes Tor nodes on proxy retry.
+
 DOWNLOADER_MIDDLEWARE = {
 	'spider.middlewares.RetryChangeProxyMiddleware': 600,
 }
-"""
-class RetryChangeProxyMiddleware(RetryMiddleware):
 
-    def _retry(self, request, reason, spider):
-        log.msg('Changing proxy')
-        tn = telnetlib.Telnet('127.0.0.1', 9051)
-        tn.read_until("Escape character is '^]'.", 2)
-        tn.write('AUTHENTICATE "267765"\r\n')
-        tn.read_until("250 OK", 2)
-        tn.write("signal NEWNYM\r\n")
-        tn.read_until("250 OK", 2)
-        tn.write("quit\r\n")
-        tn.close()
-        time.sleep(3)
-        log.msg('Proxy changed')
-        return RetryMiddleware._retry(self, request, reason, spider)
+"""
+from scrapy.contrib.downloadermiddleware.retry import RetryMiddleware
+
+
+class RetryChangeProxyMiddleware(RetryMiddleware):
+	""" DownloaderMiddleware that changes Tor proxies.
+	"""
+
+	def _retry(self, request, reason, spider):
+		log.msg('Changing proxy')
+		tn = telnetlib.Telnet('127.0.0.1', 9051)
+		tn.read_until("Escape character is '^]'.", 2)
+		tn.write('AUTHENTICATE "267765"\r\n')
+		tn.read_until("250 OK", 2)
+		tn.write("signal NEWNYM\r\n")
+		tn.read_until("250 OK", 2)
+		tn.write("quit\r\n")
+		tn.close()
+		time.sleep(3)
+		log.msg('Proxy changed')
+		return RetryMiddleware._retry(self, request, reason, spider)
