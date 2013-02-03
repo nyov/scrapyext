03c2766860be63d616904c1109037eeb447a55b6
nyov <nyov@nexnode.net>
2013-02-03 18:51:24 +0000

    add middleware description

--- scrapyext/ignorevisited.py
+++ scrapyext/ignorevisited.py
@@ -1,3 +1,10 @@
+"""
+IgnoreVisitedMiddleware
+
+Downloader middleware to ignore URLs found by previous
+LogVisitedMiddleware runs.
+"""
+
 import os.path, mmap
 from scrapy import log
 from scrapy.core import exceptions
@@ -8,7 +15,7 @@ class IgnoreVisitedMiddleware(object):
 	"""
 	def __init__(self):
 		fname = 'visited.log'
-		
+
 		if os.path.exists(fname):
 			log.msg('mmap visited log %s to ignore requests' % fname, level=log.DEBUG)
 			self.visited_map = open(fname, 'r')
@@ -17,14 +24,14 @@ class IgnoreVisitedMiddleware(object):
 			log.msg('no visited log, all requests will be processed', level=log.DEBUG)
 			self.visited = None
 			self.visisted_map = None
-	
+
 	def __del__(self):
 		if self.visisted:
 			self.visited.close()
-		
+
 		if self.visited_map:
 			self.visited_map.close()
-	
+
 	def process_request(self, request, spider):
 		# TODO: mmap.find seems pretty fast, but may want to see if re.search
 		# is faster
--- scrapyext/logvisited.py
+++ scrapyext/logvisited.py
@@ -1,3 +1,9 @@
+"""
+LogVisitedMiddleware
+
+Spider middleware to record visited URLs in a logfile.
+"""
+
 from scrapy import log, item
 
 class LogVisitedMiddleware(object):
@@ -8,17 +14,17 @@ class LogVisitedMiddleware(object):
 		fname = 'visited.log'
 		log.msg('logging visited urls to %s' % fname, level=log.INFO)
 		self.visited_log = open(fname, 'a')
-	
+
 	def __del__(self):
 		self.visited_log.close()
-	
+
 	def process_spider_output(self, response, result, spider):
 		# make sure we have a list and not an exhaustable iterable
 		results = [r for r in result]
-		
+
 		if any([isinstance(r, item.Item) for r in results]):
 			self.visited_log.write(response.url+'\n')
 			self.visited_log.flush()
 			log.msg('visited items at %s' % response.url, level=log.DEBUG)
-		
+
 		return results
