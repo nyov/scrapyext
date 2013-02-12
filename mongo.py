f45350d4837c5b9d30f85535d89e7ab23e59e114
nyov <nyov@nexnode.net>
2013-02-12 08:09:31 +0100

    MongoDBGridStorage

--- mongo.py
+++ mongo.py
@@ -8,10 +8,15 @@ ITEM_PIPELINES = [
 MONGODB_HOST = "localhost"
 MONGODB_PORT = 27017
 MONGODB_DB = "database"
+# (not for gridfs)
 MONGODB_COLLECTION = "collection"
+#MONGODB_UNIQ_KEY = "url"
 
 """
-import pymongo
+
+from pymongo import Connection
+from gridfs import GridFS
+from gridfs.errors import FileExists, GridFSError
 
 from scrapy import log
 from scrapy.conf import settings
@@ -25,7 +30,7 @@ class MongoDBStorage(object):
 		self.db = settings['MONGODB_DB']
 		self.col = settings['MONGODB_COLLECTION']
 		self.key = settings['MONGODB_UNIQ_KEY']
-		connection = pymongo.Connection(self.server, self.port)
+		connection = Connection(self.host, self.port)
 		db = connection[self.db]
 		self.collection = db[self.col]
 		if self._get_uniq_key() is not None:
@@ -58,3 +63,32 @@ class MongoDBStorage(object):
 		if not self.key or self.key == "":
 			return None
 		return self.key
+
+
+class MongoDBGridStorage(object):
+	def __init__(self):
+		self.host = settings['MONGODB_HOST']
+		self.port = settings['MONGODB_PORT']
+		self.db = settings['MONGODB_DB']
+		self.fs = {}
+		self.connection = Connection(self.host, self.port)[self.db]
+
+	def open_spider(self, spider):
+		self.fs[spider] = GridFS(self.connection, 'storage')
+
+	def close_spider(self, spider):
+		del self.fs[spider]
+
+	def process_item(self, item, spider):
+		err_msg = ''
+		for field, data in item.items():
+			if not data:
+				err_msg += 'Missing %s of item from %s\n' % (field, item['url'])
+		if err_msg:
+			raise DropItem(err_msg)
+
+		self.fs[spider].put(dict(item))
+		log.msg('Item written to MongoDB GridFS %s/%s' % (self.db, self.fs[spider]),
+			level=log.DEBUG, spider=spider)
+
+		return item
