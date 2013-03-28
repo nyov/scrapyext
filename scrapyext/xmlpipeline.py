d7fc3a7d68fc35d705238c0c22629229e18bad91
nyov <nyov@nexnode.net>
2013-03-28 09:26:33 +0000

    OrderedXmlNestedItemExporter for XmlPipeline

--- scrapyext/xmlpipeline.py
+++ scrapyext/xmlpipeline.py
@@ -1,3 +1,4 @@
+from scrapy.conf import settings
 from scrapy.contrib.exporter import XmlItemExporter
 
 # ported XmlItemExporter fix for nested items from git
@@ -17,16 +18,43 @@ class XmlNestedItemExporter(XmlItemExporter):
 		self.xg.endElement(name)
 
 
+# with fixed output ordering
+class OrderedXmlNestedItemExporter(XmlItemExporter):
+
+	# this list MUST contain all possible exporter items, currently
+	order = {'date':1, 'time':2, 'league':3, 'venue':4, 'name':5, 'score':6}
+
+	def export_item(self, item):
+		self.xg.startElement(self.item_element, {})
+		for name, value in sorted(self._get_serialized_fields(item, default_value=''), key=lambda x:self.order.__getitem__(x[0])):
+			self._export_xml_field(name, value)
+		self.xg.endElement(self.item_element)
+
+	def _export_xml_field(self, name, serialized_value):
+		self.xg.startElement(name, {})
+		if hasattr(serialized_value, 'items'):
+			for subname, value in sorted(serialized_value.items(), key=lambda x:self.order.__getitem__(x[0])):
+				self._export_xml_field(subname, value)
+		elif hasattr(serialized_value, '__iter__'):
+			for value in serialized_value:
+				self._export_xml_field('value', value)
+		else:
+			self.xg.characters(serialized_value)
+		self.xg.endElement(name)
+
+
 class XmlPipeline(object):
 
 	def __init__(self):
 		self.files = {}
+		self.path = settings['XML_OUTPUT_PATH'] or ''
 
 	def open_spider(self, spider):
-		file = open('%s.xml' % spider.name, 'w+b')
+		file = open('%s%s.xml' % (self.path, spider.name), 'w+b')
 		self.files[spider] = file
 		#self.exporter = XmlItemExporter(file, item_element='event', root_element='events')
-		self.exporter = XmlNestedItemExporter(file, item_element='event', root_element='events')
+		#self.exporter = XmlNestedItemExporter(file, item_element='event', root_element='events')
+		self.exporter = OrderedXmlNestedItemExporter(file, item_element='event', root_element='events')
 		self.exporter.start_exporting()
 
 	def close_spider(self, spider):
