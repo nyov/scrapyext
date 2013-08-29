e69702b3ccc9e2bdc894b200b276438459573a04
nyov <nyov@nexnode.net>
2013-08-29 19:33:28 +0000

    update

--- defaultsitem.py
+++ defaultsitem.py
@@ -1,13 +1,27 @@
-# reinstate default value support in newer Scrapy
-class DefaultsItem(Item):
-    """Item with default values"""
+"""
+DefaultsItem
 
-    def __getitem__(self, key):
-        try:
-            return self._values[key]
-        except KeyError:
-            field = self.fields[key]
-            if 'default' in field:
-                return field['default']
-            raise
-        return self._values[key]
+Adds default value support for nonexisting Item Fields.
+
+Example:
+
+	MyItem(Item):
+		url  = Field(default='http://example.com/')
+		id   = Field(default=0)
+		text = Field(default=None)
+
+"""
+from scrapy.item import Item
+
+
+class DefaultsItem(Item):
+	""" Item with default values """
+
+	def __getitem__(self, key):
+		try:
+			return self._values[key]
+		except KeyError:
+			field = self.fields[key]
+			if 'default' in field:
+				return field['default']
+			raise
