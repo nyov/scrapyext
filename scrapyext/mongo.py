7ae3f8612e84f4d8f09899aa0a3249a6beb6ba50
nyov <nyov@nexnode.net>
2013-12-25 20:08:30 +0000

    mongo pipeline update

--- scrapyext/mongo.py
+++ scrapyext/mongo.py
@@ -8,7 +8,7 @@ ITEM_PIPELINES = [
 MONGODB_URI = 'mongodb://localhost:27017'
 MONGODB_DATABASE = 'database'
 MONGODB_COLLECTION = 'collection'
-#MONGODB_UNIQUE_KEY = 'url'
+#MONGODB_UNIQUE_KEY = ['url', 'origin']
 
 """
 
@@ -16,26 +16,36 @@ from pymongo import errors
 from pymongo.mongo_client import MongoClient
 from pymongo.mongo_replica_set_client import MongoReplicaSetClient
 from pymongo.read_preferences import ReadPreference
+from pymongo import ASCENDING, DESCENDING
 
 from gridfs import GridFS
 from gridfs.errors import FileExists, GridFSError
 
 from scrapy import log
-from scrapy.exceptions import DropItem
+from scrapy.exceptions import NotConfigured, DropItem
 
 
 class MongoDBStorage(object):
+	""" MongoDB item pipeline """
 
 	def __init__(self, settings, stats, **kwargs):
-		self.uri = settings['MONGODB_URI'] or 'mongodb://localhost:27017'
-		self.db  = settings['MONGODB_DATABASE']
-		self.col = settings['MONGODB_COLLECTION']
+		self.stats = stats
+		if not settings.get('MONGODB_DATABASE')
+			raise NotConfigured
+		if not settings.get('MONGODB_COLLECTION')
+			raise NotConfigured
 
-		self.fsc = settings['MONGODB_FSYNC'] or False
-		self.rep = settings['MONGODB_REPLICA_SET'] or None
-		self.wcc = settings['MONGODB_WRITE_CONCERN'] or 0
+		self.uri = settings.get('MONGODB_URI', 'mongodb://localhost:27017')
+		self.db  = settings.get('MONGODB_DATABASE')
+		self.col = settings.get('MONGODB_COLLECTION')
 
-		self.key = settings['MONGODB_UNIQUE_KEY']
+		self.fsc = settings.getbool('MONGODB_FSYNC', False)
+		self.rep = settings.get('MONGODB_REPLICA_SET', None)
+		self.wcc = settings.getint('MONGODB_WRITE_CONCERN', 0)
+
+		self.key = settings.get('MONGODB_UNIQUE_KEY', None)
+		if not type(self.key) == 'list':
+			raise AttributeError
 
 	@classmethod
 	def from_crawler(cls, crawler):
@@ -61,9 +71,12 @@ class MongoDBStorage(object):
 		log.msg('Connected to MongoDB "%s", using "%s/%s"' %
 			self.uri, self.db, self.col)
 
-		# create index if not existing
-		if self._get_uniq_key() is not None:
-			self.collection.create_index(self._get_uniq_key(), unique=True)
+		# ensure index
+		# TODO: list of tuples for compound index
+		if self.key is not None:
+			key = {}
+			[key.update({k: item[k]}) for k in self.key]
+			self.collection.ensure_index(key, unique=True)
 
 	def close_spider(self, spider):
 		del self.collection
@@ -73,68 +86,65 @@ class MongoDBStorage(object):
 	def process_item(self, item, spider):
 		self.stats.inc_value('mongostorage/called_count')
 
-		dictitem = item.copy() # don't modify original
-		if not isinstance(item, list):
-			dictitem = dict(item)
+		mongoitem = item.copy() # don't modify original
+		mongoitem = self.verify_item(mongoitem, spider)
 
-		self.verify_item(item, spider)
+		dictitem = mongoitem
+		if not isinstance(mongoitem, list):
+			dictitem = dict(mongoitem)
 
 		# do not insert None/null values
 		for idx, v in dictitem.items():
 			if v is None:
 				del dictitem[idx]
 
-		if self._get_uniq_key() is None:
+		if self.key is None #and dictitem['unique_key'] is None:
 			try:
 				self.collection.insert(dictitem, continue_on_error=True)
-				self.stats.inc_value('mongostorage/insert_count')
 				log.msg('Item inserted in MongoDB database %s/%s' % (self.db, self.col),
 					level=log.DEBUG, spider=spider)
+				self.stats.inc_value('mongostorage/insert_count')
 			except errors.DuplicateKeyError:
 				self.stats.inc_value('mongostorage/dupkey_error_count')
 		else:
-			if self.collection.find_one({ self._get_uniq_key(): dictitem[self._get_uniq_key()] }):
-				log.msg('Item already exists in MongoDB %s' % (dictitem[self._get_uniq_key()]), level=log.DEBUG, spider=spider)
+			key = {}
+			[key.update({k: dictitem[k]}) for k in self._unique_key]
+
+			if self.collection.find_one(key):
+				log.msg('Item already exists in MongoDB for key %s' % key,
+					level=log.DEBUG, spider=spider)
+				self.stats.inc_value('mongostorage/dupkey_error_count')
 				return item
 
-			self.collection.update(
-				{self._get_uniq_key(): item[self._get_uniq_key()]},
-				dictitem,
-				upsert=True)
-			self.stats.inc_value('mongostorage/upsert_count')
+			# TODO: check return value for success?
+			self.collection.update(key, dictitem, upsert=True)
 			log.msg('Item upserted in MongoDB database %s/%s' % (self.db, self.col),
 				level=log.DEBUG, spider=spider)
+			self.stats.inc_value('mongostorage/upsert_count')
 
-	#	log.msg('Item written to MongoDB database %s/%s' % (self.db, self.col),
-	#		level=log.DEBUG, spider=spider)
+		self.stats.inc_value('mongostorage/success_count')
 
 		del dictitem
 		return item
 
 	def verify_item(self, item, spider):
 		""" Verify item before processing.
+
+		Override in subclass.
 		"""
-		err_msg = ''
-		for field, data in item.items():
-			if not data:
-				err_msg += 'Missing %s in item\n' % (field)
-		if err_msg:
-			err_msg += 'from %s' % (item)
-			log.msg(err_msg, spider=spider, level=log.INFO)
-			raise DropItem(err_msg)
+		return item
 
 	def insert_item(self, item, spider):
 		""" Insert database item.
+
+		Override in subclass.
 		"""
 
 	def update_item(self, item, spider):
 		""" Update/upsert database item.
-		"""
 
-	def _get_uniq_key(self):
-		if not self.key or self.key == "":
-			return None
-		return self.key
+		Override in subclass.
+		"""
 
 
 class ProductStorage(MongoDBStorage):
@@ -146,6 +156,20 @@ class ProductStorage(MongoDBStorage):
 		self.col = settings['MONGODB_PRODUCT_COLLECTION']
 		self.key = settings['MONGODB_PRODUCT_UNIQUE_KEY']
 
+	def verify_item(self, item, spider):
+		err_msg = ''
+		for field, data in item.items():
+			if field == 'content' and not data:
+				err_msg += 'Missing %s in item\n' % (field)
+		if err_msg:
+			err_msg += 'from %s' % (item)
+			log.msg(err_msg, spider=spider, level=log.DEBUG)
+			#raise DropItem(err_msg)
+			return
+
+		return item
+
+
 class ManufacturerStorage(MongoDBStorage):
 
 	def __init__(self, settings, stats, **kwargs):
@@ -155,17 +179,34 @@ class ManufacturerStorage(MongoDBStorage):
 		self.col = settings['MONGODB_MANUFAC_COLLECTION']
 		self.key = settings['MONGODB_MANUFAC_UNIQUE_KEY']
 
+	def verify_item(self, item, spider):
+		err_msg = ''
+		for field, data in item.items():
+			if not data:
+				err_msg += 'Missing %s in item\n' % (field)
+		if err_msg:
+			err_msg += 'from %s' % (item)
+			log.msg(err_msg, spider=spider, level=log.DEBUG)
+			#raise DropItem(err_msg)
+			return
+
+		return item
+
 
 class MongoDBGridStorage(object):
 
 	def __init__(self, settings, stats, **kwargs):
-		self.uri = settings['MONGODB_URI'] or 'mongodb://localhost:27017'
-		self.db	 = settings['MONGODB_DATABASE']
-		self.col = settings['MONGODB_COLLECTION'] or 'fs'
+		self.stats = stats
+		if not settings.get('MONGODB_DATABASE')
+			raise NotConfigured
 
-		self.fsc = settings['MONGODB_FSYNC'] or False
-		self.rep = settings['MONGODB_REPLICA_SET'] or None
-		self.wcc = settings['MONGODB_WRITE_CONCERN'] or 0
+		self.uri = settings.get('MONGODB_URI', 'mongodb://localhost:27017')
+		self.db  = settings.get('MONGODB_DATABASE')
+		self.col = settings.get('MONGODB_COLLECTION', 'fs')
+
+		self.fsc = settings.getbool('MONGODB_FSYNC', False)
+		self.rep = settings.get('MONGODB_REPLICA_SET', None)
+		self.wcc = settings.getint('MONGODB_WRITE_CONCERN', 0)
 
 		self.fs = {}
 
@@ -199,6 +240,29 @@ class MongoDBGridStorage(object):
 		#del self.database
 
 	def process_item(self, item, spider):
+		mongoitem = self.verify_item(item, spider)
+		if not mongoitem:
+			return item
+
+		try:
+			self.fs[spider].put(dict(mongoitem))
+			log.msg('Item written to MongoDB GridFS %s/%s' %
+				(self.db, self.fs),
+				level=log.DEBUG, spider=spider)
+			self.stats.inc_value('mongostorage/gridfs_insert_count')
+		except FileExists:
+			self.stats.inc_value('mongostorage/gridfs_duplicate_count')
+		except GridFSError:
+			self.stats.inc_value('mongostorage/gridfs_error_count')
+			raise
+
+		return item
+
+	def verify_item(self, item, spider):
+		""" Verify item before processing.
+
+		Override in subclass.
+		"""
 		err_msg = ''
 		for field, data in item.items():
 			if not data:
@@ -206,10 +270,5 @@ class MongoDBGridStorage(object):
 		if err_msg:
 			err_msg += 'from %s' % (item)
 			raise DropItem(err_msg)
-
-		self.fs[spider].put(dict(item))
-
-		log.msg('Item written to MongoDB GridFS %s/%s' % (self.db, self.fs[spider]),
-			level=log.DEBUG, spider=spider)
 
 		return item
