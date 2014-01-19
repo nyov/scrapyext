926a721520bfbdaae25693985da1e99b67aa05e9
nyov <nyov@nexnode.net>
2014-01-19 16:59:41 +0000

    merged request_fingerprint hacks

--- scrapyext/dupefilter.py
+++ scrapyext/dupefilter.py
@@ -4,14 +4,11 @@ import weakref
 import re
 
 from scrapy.utils.url import canonicalize_url
-from scrapy.dupefilter import RFPDupeFilter
 
 _fingerprint_cache = weakref.WeakKeyDictionary()
 def request_fingerprint(request, include_headers=None):
 	"""
-	Return the request fingerprint.
-
-	From a modified request, without modifying the Request.
+	Calculate request fingerprint on a modified request.
 	"""
 	if include_headers:
 		include_headers = tuple([h.lower() for h in sorted(include_headers)])
@@ -22,12 +19,13 @@ def request_fingerprint(request, include_headers=None):
 		# hack here #
 		# * filter incrementing 'requestid' from url
 		rewritten_url = canonicalize_url(request.url)
-		rewritten_url = re.sub(r'(?:requestid=\d+)', '', rewritten_url)
+		#rewritten_url = re.sub(r'(?:requestid=\d+)', '', rewritten_url)
 		fp.update(rewritten_url)
-		#parsed_body = re.sub(r'(httpSessionId=.*?\n)', '', request.body)
+		# * ignore sessionid from xhr post body
+		parsed_body = request.body or ''
+		#parsed_body = re.sub(r'(httpSessionId=.*?\n)', '', parsed_body)
 		#parsed_body = re.sub(r'(scriptSessionId=.*?\n)', '', parsed_body)
-		#fp.update(parsed_body or '')
-		fp.update(request.body or '')
+		fp.update(parsed_body or '')
 		# end hack #
 		if include_headers:
 			for hdr in include_headers:
@@ -39,13 +37,20 @@ def request_fingerprint(request, include_headers=None):
 	return cache[include_headers]
 
 
-class DupeFilter(RFPDupeFilter):
+#import scrapy.utils.request
+#scrapy.utils.request.request_fingerprint = request_fingerprint
+#from scrapy.utils.request import request_fingerprint
+
+
+from scrapy.dupefilter import RFPDupeFilter as _RFPDupeFilter
+
+class RFPDupeFilter(_RFPDupeFilter):
 
 	#def __init__(self, path=None):
-	#	super(DupeFilter, self).__init__(path)
+	#	super(RFPDupeFilter, self).__init__(path)
 
 	def request_seen(self, request):
-	#	super(DupeFilter, self).request_seen(request)
+	#	super(RFPDupeFilter, self).request_seen(request)
 		fp = request_fingerprint(request)
 		if fp in self.fingerprints:
 			return True
@@ -53,3 +58,14 @@ class DupeFilter(RFPDupeFilter):
 		if self.file:
 			self.file.write(fp + os.linesep)
 
+
+from scrapy.contrib.httpcache import FilesystemCacheStorage as _FilesystemCacheStorage
+
+class FilesystemCacheStorage(_FilesystemCacheStorage):
+
+	def __init__(self, *args, **kwargs):
+		super(FilesystemCacheStorage, self).__init__(*args, **kwargs)
+
+	def _get_request_path(self, spider, request):
+		key = request_fingerprint(request)
+		return os.path.join(self.cachedir, spider.name, key[0:2], key)
--- scrapyext/httpcache.py
+++ scrapyext/httpcache.py
@@ -8,9 +8,7 @@ from scrapy.utils.url import canonicalize_url
 _fingerprint_cache = weakref.WeakKeyDictionary()
 def request_fingerprint(request, include_headers=None):
 	"""
-	Return the request fingerprint.
-
-	Ignore Session IDs for fingerprinting (in POST body)
+	Calculate request fingerprint on a modified request.
 	"""
 	if include_headers:
 		include_headers = tuple([h.lower() for h in sorted(include_headers)])
@@ -18,11 +16,15 @@ def request_fingerprint(request, include_headers=None):
 	if include_headers not in cache:
 		fp = hashlib.sha1()
 		fp.update(request.method)
-		fp.update(canonicalize_url(request.url))
 		# hack here #
-		# special xhr post body, ignore the sessionid
-		parsed_body = re.sub(r'(httpSessionId=.*?\n)', '', request.body)
-		parsed_body = re.sub(r'(scriptSessionId=.*?\n)', '', parsed_body)
+		# * filter incrementing 'requestid' from url
+		rewritten_url = canonicalize_url(request.url)
+		#rewritten_url = re.sub(r'(?:requestid=\d+)', '', rewritten_url)
+		fp.update(rewritten_url)
+		# * ignore sessionid from xhr post body
+		parsed_body = request.body or ''
+		#parsed_body = re.sub(r'(httpSessionId=.*?\n)', '', parsed_body)
+		#parsed_body = re.sub(r'(scriptSessionId=.*?\n)', '', parsed_body)
 		fp.update(parsed_body or '')
 		# end hack #
 		if include_headers:
@@ -35,13 +37,30 @@ def request_fingerprint(request, include_headers=None):
 	return cache[include_headers]
 
 
-# mock patch?
-# (also used by RFPDupeFilter)
 #import scrapy.utils.request
 #scrapy.utils.request.request_fingerprint = request_fingerprint
+#from scrapy.utils.request import request_fingerprint
+
+
+from scrapy.dupefilter import RFPDupeFilter as _RFPDupeFilter
+
+class RFPDupeFilter(_RFPDupeFilter):
+
+	#def __init__(self, path=None):
+	#	super(RFPDupeFilter, self).__init__(path)
+
+	def request_seen(self, request):
+	#	super(RFPDupeFilter, self).request_seen(request)
+		fp = request_fingerprint(request)
+		if fp in self.fingerprints:
+			return True
+		self.fingerprints.add(fp)
+		if self.file:
+			self.file.write(fp + os.linesep)
 
 
 from scrapy.contrib.httpcache import FilesystemCacheStorage as _FilesystemCacheStorage
+
 class FilesystemCacheStorage(_FilesystemCacheStorage):
 
 	def __init__(self, *args, **kwargs):
