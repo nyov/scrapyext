28e0ebdcbcc3a876fbebd38936ac305abd64f880
nyov <nyov@nexnode.net>
2014-01-20 22:50:14 +0000

    magic variables sql syntax

--- sql.py
+++ sql.py
@@ -18,6 +18,42 @@ class MyItem(SQLItem):
 	spider = Field()
 ```
 
+Prepared statements using magic variables:
+
+ $table
+	item database tablename
+	will be replaced by SQLItem's __tablename__ or item class name
+
+	$table:escaped
+	- tablename will be escaped (default)
+ $fields (table cells)
+	item fields (keys), escaped
+	will be replaced by fields found in item
+	- concatenated by comma (,)
+	(equals `$fields:,` )
+
+	$fields:and
+	- concatenated by ' AND '
+ $values
+	item field's scraped contents
+	will be replaced by items' $fields respective values
+	- concatenated by comma (,)
+ $fields_values
+	item $fields and their respective $values (as above),
+	joined by '='. ($field1=$value1, $field2=$value2, ...)
+	- concatenated by comma (,)
+
+	$fields_values:and
+	- concatenated by ' AND '
+ $indices
+ $indexes
+	item's unique field(s) if defined
+	- concatenated by ','
+
+	$indexes:and (THIS IS USUALLY WHAT YOU WANT in the "WHERE" clause)
+	- concatenated by ' AND '
+	  (This means all UniqueFields are counted as a single constraint)
+
 """
 from scrapy import log
 from scrapy import signals
@@ -30,6 +66,98 @@ from collections import OrderedDict
 import re
 
 
+_ENTITIES_RE = re.compile(r'(\$[a-z_]+)(:[\w,]+)?')
+
+def _sql_format(query, item, paramstyle=':', identifier='"'):
+	"""Prepared statements for Item
+
+	(Notice, not every dbapi driver does actually prepare
+	 statements in-database, only escapes them beforehand
+	 (e.g. MySQLdb))
+	"""
+	def paramformat(value):
+		# simple or numbered params?
+		if not isinstance(value, int):
+			raise ValueError('Integer required.')
+		if paramstyle == '?' or paramstyle == '%s':
+			return paramstyle
+		else:
+			return paramstyle + str(value)
+
+	tablename = item.__tablename__ or item.__class__.__name__ #.lower().rstrip('item')
+
+	indices = []
+	for n, v in item.fields.iteritems():
+		if isinstance(v, UniqueField):
+			indices += [n]
+
+	# convert to preserve item order from here on out
+	item = OrderedDict(item)
+
+	# if this seems to be a destructive operation, check for missing indices
+	if query[:6].lower() not in ('select'):
+		for val in (v for v in indices if v not in item.keys()):
+			raise ValueError('Item missing value for UniqueField "%s" but index must be present.' % val)
+
+	# order indices
+	i = []
+	for key in (k for k in item.keys() if k in indices):
+		i.append(key)
+	indices = i
+
+	itemkeys   = ['{0}{1}{0}'.format(identifier, f) for f in indices] # escaping
+	itemfields = ['{0}{1}{0}'.format(identifier, f) for f in item.keys()] # escaping
+
+	itemvalues = []
+
+	prepared = query
+	for m in _ENTITIES_RE.finditer(query):
+		entity, args = m.groups()
+		args = (args or ':')[1:]
+		field = value = None
+		if entity == '$table':
+			field = tablename
+			if args[:3] == 'esc':
+				field = '{0}{1}{0}'.format(identifier, tablename)
+		elif entity == '$fields':
+			attr = ','
+			if args:
+				attr = ' %s ' % args.upper()
+			field = attr.join(itemfields)
+		elif entity == '$values':
+			attr = ','
+			if args:
+				attr = ' %s ' % args.upper()
+			field = attr.join([paramformat(i) for i, _ in enumerate(itemfields)])
+			# FIXME: handle this better?
+			value = item.values()
+		elif entity == '$fields_values':
+			attr = ','
+			if args:
+				attr = ' %s ' % args.upper()
+			values = ['%s=%s' % (k, paramformat(i)) for i, k in enumerate(itemfields)]
+			field = attr.join(values)
+			# FIXME: handle this better?
+			value = item.values()
+		elif entity == '$indices' or entity == '$indexes':
+			attr = ','
+			if args:
+				attr = ' %s ' % args.upper()
+			indexes = ['%s=%s' % (idx, paramformat(i)) for i, idx in enumerate(itemkeys)]
+			field = attr.join(indexes)
+			# FIXME: handle this better?
+			value = [v for k, v in item.iteritems() if k in indices]
+		elif entity:
+			raise ValueError('Invalid entity in SQL_QUERIES: "%s"' % entity)
+
+		if field is not None:
+			prepared = prepared.replace(m.group(), field, 1)
+			if value is not None:
+				itemvalues.extend(value)
+
+	return prepared, itemvalues
+
+
 class UniqueField(Field):
 	"""Field to tell SQLPipeline about an index.
 	"""
@@ -39,8 +167,6 @@ class SQLItem(Item):
 	"""
 	__tablename__ = ''
 
-
-_ENTITIES_RE = re.compile(r'(\$[a-z_]+)(:[\w,]+)?')
 
 class SQLPipeline(object):
 
@@ -52,6 +178,7 @@ class SQLPipeline(object):
 
 		self.settings = settings
 		self.paramstyle = ':'
+		self.identifier = '"' # ANSI
 
 		if self.settings.get('drivername') == 'sqlite':
 			self.__dbpool = ConnectionPool('sqlite3', self.settings.get('database', ':memory:'),
@@ -95,6 +222,7 @@ class SQLPipeline(object):
 				cp_reconnect = True,
 			)
 			self.paramstyle = '%s'
+			self.identifier = '`' # MySQL
 
 		self.queries = kwargs.get('queries')
 
@@ -103,58 +231,21 @@ class SQLPipeline(object):
 		if not crawler.settings.get('DATABASE'):
 			raise NotConfigured('No database connection settings found.')
 
-		# Prepared statements using magic variables
-		# supported fields:
-		#
-		# $table
-		#	item database tablename
-		#	will be replaced by SQLItem's __tablename__ or item class name
-		#
-		#	$table:escaped
-		#	- tablename will be escaped (default)
-		# $fields (table cells)
-		#	item fields (keys), escaped
-		#	will be replaced by fields found in item
-		#	- concatenated by comma (,)
-		#	(equals `$fields:,` )
-		#
-		#	$fields:and
-		#	- concatenated by ' AND '
-		# $values
-		#	item field's scraped contents
-		#	will be replaced by items' $fields respective values
-		#	- concatenated by comma (,)
-		# $fields_values
-		#	item $fields and their respective $values (as above),
-		#	joined by '='. ($field1=$value1, $field2=$value2, ...)
-		#	- concatenated by comma (,)
-		#
-		#	$fields_values:and
-		#	- concatenated by ' AND '
-		# $indices
-		# $indexes
-		#	item's unique field(s) if defined
-		#	- concatenated by ','
-		#
-		#	$indexes:and (THIS IS USUALLY WHAT YOU WANT in the "WHERE" clause)
-		#	- concatenated by ' AND '
-		#	  (This means all UniqueFields are counted as a single constraint)
-		#
-		SQL_QUERIES = [
-			('select', "SELECT $fields FROM $table:esc"),
-			('insert', "INSERT INTO $table:esc ($fields) VALUES ($values)"),
-		#	('insert', "INSERT INTO $table:esc SET $fields_values"), #! sqlite
-		#	('upsert', "INSERT OR REPLACE INTO $table:esc ($fields) VALUES ($values)"), #! sqlite
-			('upsert', "REPLACE INTO $table ($fields) VALUES ($values)"), #! mysql
-		#	('upsert', "INSERT INTO $table:esc SET $fields_values ON DUPLICATE KEY UPDATE $fields_values"),
-			('deleteme', "DELETE FROM $table:esc WHERE $fields_values:and"), # exact item match required
+		SQL_QUERIES = {
+			'select': "SELECT $fields FROM $table:esc",
+			'insert': "INSERT INTO $table:esc ($fields) VALUES ($values)",
+		#	'insert': "INSERT INTO $table:esc SET $fields_values", #! sqlite
+		#	'upsert': "INSERT OR REPLACE INTO $table:esc ($fields) VALUES ($values)", #! sqlite
+			'upsert': "REPLACE INTO $table ($fields) VALUES ($values)", #! mysql
+		#	'upsert': "INSERT INTO $table:esc SET $fields_values ON DUPLICATE KEY UPDATE $fields_values",
+			'deleteme': "DELETE FROM $table:esc WHERE $fields_values:and", # exact item match required
 			# have indices?
-			('update', "UPDATE $table:esc SET $fields_values WHERE $indices:and"),
-			('fetchall', "SELECT $fields FROM $table:esc WHERE $indices:and"), # select on unique keys
-			('fetchone', "SELECT $fields FROM $table:esc WHERE $indices:and LIMIT 1"), # if backend supports LIMIT
-			('delete', "DELETE FROM $table:esc WHERE $indices:and"), # match on unique fields
+			'update': "UPDATE $table:esc SET $fields_values WHERE $indices:and",
+			'fetchall': "SELECT $fields FROM $table:esc WHERE $indices:and", # select on unique keys
+			'fetchone': "SELECT $fields FROM $table:esc WHERE $indices:and LIMIT 1", # if backend supports LIMIT
+			'delete': "DELETE FROM $table:esc WHERE $indices:and", # match on unique fields
 			# ... (add your own)
-		]
+		}
 		# crawler.settings.get('SQL_QUERIES')
 
 		o = cls(settings=crawler.settings.get('DATABASE'), stats=crawler.stats, queries=SQL_QUERIES)
@@ -178,21 +269,6 @@ class SQLPipeline(object):
 	def process_item(self, item, spider):
 		"""Process the item."""
 
-		# Only process items inheriting SQLItem
-		if not isinstance(item, SQLItem):
-			return item
-
-		query, values = self.sql_for(item, 'upsert')
-
-		deferred = self.operation((query[0], values), item, spider)
-		deferred.addErrback(self._database_error, item, spider)
-
-		# always return item
-		deferred.addBoth(lambda _: item)
-
-		return deferred
-
-	def operation(self, op, item, spider):
 		def onerror(error, query, params):
 			for p in params:
 				query = re.sub('(\\'+self.paramstyle+r'\d?)', '"%s"' % p, query, count=1)
@@ -203,114 +279,82 @@ class SQLPipeline(object):
 				query = re.sub('(\\'+self.paramstyle+r'\d?)', '"%s"' % p, query, count=1)
 			log.msg('%s executed: %s' % (self.__class__.__name__, query), level=log.DEBUG, spider=spider)
 
-		query, params = op
+		# Only process items inheriting SQLItem
+		if not isinstance(item, SQLItem):
+			return item
 
-		deferred = self.__dbpool.runOperation(query, params)
+	#	query, params = _sql_format(self.queries['upsert'], item, paramstyle=self.paramstyle, identifier=self.identifier)
+		query, params = _sql_format(self.queries['insert'], item, paramstyle=self.paramstyle, identifier=self.identifier)
+		update, uvalues = _sql_format(self.queries['update'], item, paramstyle=self.paramstyle, identifier=self.identifier)
+
+	#	deferred = self.operation((query, params), item, spider)
+	#	deferred.addCallback(onsuccess, query, params)
+	#	deferred.addErrback(onerror, query, params)
+	#	deferred.addErrback(self._database_error, item, spider)
+
+		deferred = self.insert_update((query, params), (update, uvalues), item, spider)
 		deferred.addCallback(onsuccess, query, params)
-		deferred.addErrback(onerror, query, params)
-		#deferred.addErrback(self._database_error, item, spider)
+		#deferred.addErrback(onerror, query, params)
+		deferred.addErrback(onerror, update, uvalues)
+		deferred.addErrback(self._database_error, item, spider)
+
+		# always return item
+		deferred.addBoth(lambda _: item)
+
 		return deferred
 
-	def sql_for(self, item, querytype=None):
-		"""Prepared statements for Item
+		'''
+		deferred = self.__dbpool.runInteraction(self.transaction, item, spider)
+	#	deferred.addCallback(onsuccess, query, params)
+	#	deferred.addErrback(onerror, query, params)
+		deferred.addErrback(self._database_error, item, spider)
 
-		(notice, not every dbapi driver does actually prepare
-		 statements in database, only escapes them (e.g. MySQLdb))
-		"""
-		def paramstyle(value):
-			if not isinstance(value, int):
-				raise ValueError('Integer required.')
-			if self.paramstyle == '?' or self.paramstyle == '%s':
-				return self.paramstyle
-			else:
-				return self.paramstyle + str(value)
+		# always return item
+		deferred.addBoth(lambda _: item)
 
-		tablename = item.__tablename__ or item.__class__.__name__ #.lower().rstrip('item')
+		return deferred
+		'''
 
-		indices = []
-		for n, v in item.fields.iteritems():
-			if isinstance(v, UniqueField):
-				indices += [n]
+	def operation(self, query, params, item, spider):
+		deferred = self.__dbpool.runOperation(query, params)
+		return deferred
 
-		# convert to preserve item order from here on out
-		item = OrderedDict(item)
+	def insert_update(self, insert, update, item, spider):
+		import MySQLdb
+		def upsert(result, query, params):
+			result.trap(MySQLdb.IntegrityError)
+			spider.log('%s executing: %s' % (self.__class__.__name__, query), level=log.ERROR)
+			deferred = self.__dbpool.runOperation(query, params)
+			return deferred
 
-		# if this seems to be a destructive operation, check for missing indices
-		if querytype.lower() not in ('select'):
-			for val in (v for v in indices if v not in item.keys()):
-				raise ValueError('Item missing value for UniqueField "%s" but index must be present.' % val)
-				#raise DropItem('Item missing value for UniqueField "%s" but index must be present.' % val)
+		query, params = insert
+		uquery, uparams = update
+		deferred = self.__dbpool.runOperation(query, params)
+		deferred.addErrback(upsert, uquery, uparams)
+		return deferred
 
-		# order indices
-		i = []
-		for key in (k for k in item.keys() if k in indices):
-			i.append(key)
-		indices = i
+	def transaction(self, txn, item, spider):
+		# This will run in a thread, we can use blocking calls
 
-		identifier = '"' # ANSI
-		if self.settings.get('drivername') == 'mysql':
-			identifier = '`'
-		itemkeys   = ['{0}{1}{0}'.format(identifier, f) for f in indices] # escaping
-		itemfields = ['{0}{1}{0}'.format(identifier, f) for f in item.keys()] # escaping
+		import MySQLdb
 
-		# return this for matching order with prepared statement on execution
-		itemvalues = []
+		# run INSERT, UPDATE on failure
+		# (mysql wont error trying to update nonexistant column :()
+		try:
+			query, params = _sql_format(self.queries['insert'], item, paramstyle=self.paramstyle, identifier=self.identifier)
+			spider.log("SQL: %s | %s" % (query, params), level=log.DEBUG)
+			txn.execute(query, params)
+		#except sqlite3.IntegrityError:
+		except MySQLdb.IntegrityError:
+			query, params = _sql_format(self.queries['update'], item, paramstyle=self.paramstyle, identifier=self.identifier)
+			spider.log("SQL: %s | %s" % (query, params), level=log.DEBUG)
+			txn.execute(query, params)
 
-		prepared_statements = []
-		for query, statement in self.queries:
-			if querytype and query != querytype:
-				continue
-			final = statement
-			for m in _ENTITIES_RE.finditer(statement):
-				entity, args = m.groups()
-				args = (args or ':')[1:]
-				field = value = None
-				if entity == '$table':
-					field = tablename
-					if args[:3] == 'esc':
-						field = '{0}{1}{0}'.format(identifier, tablename)
-				elif entity == '$fields':
-					attr = ','
-					if args:
-						attr = ' %s ' % args.upper()
-					field = attr.join(itemfields)
-				elif entity == '$values':
-					attr = ','
-					if args:
-						attr = ' %s ' % args.upper()
-					field = attr.join([paramstyle(i) for i, _ in enumerate(itemfields)])
-					# FIXME: handle this better
-					value = item.values()
-				elif entity == '$fields_values':
-					attr = ','
-					if args:
-						attr = ' %s ' % args.upper()
-					values = ['%s=%s' % (k, paramstyle(i)) for i, k in enumerate(itemfields)]
-					field = attr.join(values)
-					# FIXME: handle this better
-					value = item.values()
-				elif entity == '$indices' or entity == '$indexes':
-					attr = ','
-					if args:
-						attr = ' %s ' % args.upper()
-					indexes = ['%s=%s' % (idx, paramstyle(i)) for i, idx in enumerate(itemkeys)]
-					field = attr.join(indexes)
-					# FIXME: handle this better
-					value = [v for k, v in item.iteritems() if k in indices]
-				elif entity:
-					raise ValueError('Invalid entity in SQL_QUERIES: "%s"' % entity)
-
-				if field is not None:
-					final = final.replace(m.group(), field, 1)
-					if value is not None:
-						itemvalues.extend(value)
-
-			if querytype:
-				prepared_statements.append(final)
-			else:
-				prepared_statements.append((query, final))
-
-		return prepared_statements, itemvalues
+		# primary key check
+	#	tx.execute(query, (item['url']))
+	#	result = tx.fetchone()
+	#	if result:
+	#		log.msg("Item already in db: (id) %s item:\n%r" % (result['id'], item), level=log.DEBUG)
 
 	def _database_error(self, e, item, spider=None):
 		"""Log exceptions."""
