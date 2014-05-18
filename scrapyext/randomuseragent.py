54faf011797591c195b1e4af4f05238fc2fcd634
nyov <nyov@nexnode.net>
2014-05-18 04:33:02 +0000

    update RandomUserAgentMiddleware

--- scrapyext/randomuseragent.py
+++ scrapyext/randomuseragent.py
@@ -1,32 +1,54 @@
 """
 Use a random user agent for each request
 
-You can use this middleware to have a random user agent every request the spider makes.
-You can define a user USER_AGEN_LIST in your settings and the spider will chose a random user agent from that list every time.
+(This is a "bad" middleware, please do not use unless necessary. Be nice to your sources!)
 
-You will have to disable the default user agent middleware and add this to your settings file.
+You can use this middleware to have a random user agent on every request the spider makes.
+You can define a USER_AGENTS list in your settings and the spider will chose a random user agent from that list every time.
+
+You should disable the default user agent middleware and add this instead.
 
     DOWNLOADER_MIDDLEWARES = {
-        'scrapylib.random_useragent.RandomUserAgentMiddleware': 400,
+        'scrapyext.randomuseragent.RandomUserAgentMiddleware': 400,
         'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
     }
 
-imported from
-http://snipplr.com/view/66992/use-a-random-user-agent-for-each-request/
-
-# Snippet imported from snippets.scrapy.org (which no longer works)
-# author: dushyant
-# date  : Sep 16, 2011
+    USER_AGENTS = [
+        "Your",
+	"User",
+	"Agents",
+    ]
 """
 
 import random
-from scrapy import log
-from project.settings import USER_AGENT_LIST
 
 class RandomUserAgentMiddleware(object):
 
-    def process_request(self, request, spider):
-        ua = random.choice(USER_AGENT_LIST)
-        if ua:
-            request.headers.setdefault('User-Agent', ua)
-        #log.msg('>>>> UA %s'%request.headers)
+	def __init__(self, agents):
+		self.agents = agents
+
+	@classmethod
+	def from_crawler(cls, crawler):
+		default_agents = [
+			"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+			"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+			"Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+			"Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+			"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+			"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+			"Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+			"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+			"Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+			"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+			"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+			"Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+			"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+			"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+		]
+		# use provided list from settings or fall back to included list
+		return cls(crawler.settings.getlist('USER_AGENTS', default_agents))
+
+	def process_request(self, request, spider):
+		request.headers.setdefault('User-Agent', random.choice(self.agents))
