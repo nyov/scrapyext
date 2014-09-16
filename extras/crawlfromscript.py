fcc26931cb47997b3c8f786ce9d71127041a2373
nyov <nyov@nexnode.net>
2014-09-16 04:59:02 +0000

    updating bitrot

--- extras/crawlfromscript.py
+++ extras/crawlfromscript.py
@@ -31,42 +31,42 @@ from multiprocessing import Process, Queue
 
 class CrawlerScript():
 
-	def __init__(self):
-		self.crawler = CrawlerProcess(settings)
-		if not hasattr(project, 'crawler'):
-			self.crawler.install()
-		self.crawler.configure()
-		self.items = []
-		dispatcher.connect(self._item_passed, signals.item_passed)
+    def __init__(self):
+        self.crawler = CrawlerProcess(settings)
+        if not hasattr(project, 'crawler'):
+            self.crawler.install()
+        self.crawler.configure()
+        self.items = []
+        dispatcher.connect(self._item_passed, signals.item_passed)
 
-	def _item_passed(self, item):
-		self.items.append(item)
+    def _item_passed(self, item):
+        self.items.append(item)
 
-	def _crawl(self, queue, spider_name):
-		spider = self.crawler.spiders.create(spider_name)
-		if spider:
-			self.crawler.queue.append_spider(spider)
-		self.crawler.start()
-		self.crawler.stop()
-		queue.put(self.items)
+    def _crawl(self, queue, spider_name):
+        spider = self.crawler.spiders.create(spider_name)
+        if spider:
+            self.crawler.queue.append_spider(spider)
+        self.crawler.start()
+        self.crawler.stop()
+        queue.put(self.items)
 
-	def crawl(self, spider):
-		queue = Queue()
-		p = Process(target=self._crawl, args=(queue, spider,))
-		p.start()
-		p.join()
-		return queue.get(True)
+    def crawl(self, spider):
+        queue = Queue()
+        p = Process(target=self._crawl, args=(queue, spider,))
+        p.start()
+        p.join()
+        return queue.get(True)
 
 # Usage
 if __name__ == "__main__":
-	log.start()
+    log.start()
 
-	"""
-	This example runs spider1 and then spider2 three times.
-	"""
-	items = list()
-	crawler = CrawlerScript()
-	items.append(crawler.crawl('spider1'))
-	for i in range(3):
-		items.append(crawler.crawl('spider2'))
-	print items
+    """
+    This example runs spider1 and then spider2 three times.
+    """
+    items = list()
+    crawler = CrawlerScript()
+    items.append(crawler.crawl('spider1'))
+    for i in range(3):
+        items.append(crawler.crawl('spider2'))
+    print items
