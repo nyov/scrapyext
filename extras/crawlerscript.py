2c8753efcd29770b0227b0c6144cee7455371e9c
nyov <nyov@nexnode.net>
2014-10-09 09:39:09 +0000

    run crawler from script

---
+++ extras/crawlerscript.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+
+class MySpider(scrapy.Spider):
+	"""Some Spider"""
+
+	name = 'my_spider'
+
+
+if __name__ == "__main__":
+	# for scrapy 0.24
+	from twisted.internet import reactor
+	from scrapy.utils.project import get_project_settings
+	from scrapy.crawler import Crawler
+	from scrapy import log, signals
+
+	spider = MySpider()
+	settings = get_project_settings()
+	crawler = Crawler(settings)
+	crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+	crawler.configure()
+	crawler.crawl(spider)
+	crawler.start()
+	log.start()
+	reactor.run() # the script will block here until the spider_closed signal was sent
