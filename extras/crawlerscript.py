3ff4caf1c48816d0756f28706d5a0178dcd6491c
nyov <nyov@nexnode.net>
2015-07-11 14:59:32 +0000

    crawlscript, multiple spiders in process

--- extras/crawlerscript.py
+++ extras/crawlerscript.py
@@ -21,13 +21,27 @@ if __name__ == "__main__":
     from scrapy.crawler import Crawler
     from scrapy import log, signals
 
-    spider = MySpider(stuff='stuff')
-    settings = Settings()
-    #settings.setdict(env_overrides, priority='project')
-    crawler = Crawler(settings)
-    crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-    crawler.configure()
-    crawler.crawl(spider)
-    crawler.start()
+    global crawlercount
+    crawlercount = 0
+    def crawlstack():
+        global crawlercount
+        crawlercount -= 1
+        if crawlercount < 1:
+            reactor.stop()
+
+    def setup_crawler(stuff):
+        spider = MySpider(stuff=stuff)
+        settings = Settings()
+        #settings.setdict(env_overrides, priority='project')
+        crawler = Crawler(settings)
+        crawler.signals.connect(crawlstack, signal=signals.spider_closed)
+        crawler.configure()
+        crawler.crawl(spider)
+        crawler.start()
+
+    for stuff in ['stuff1', 'stuff2']:
+        crawlercount += 1
+        setup_crawler(stuff)
+
     log.start()
     reactor.run() # the script will block here until the spider_closed signal was sent
